40dbafb0ba2974b374124a5303db80f57d8bbe1c
Fraser <tomatoesneverlie@gmail.com>
2020-04-19 22:07:03 +0200

    Made camera more like jovi's vision

--- LDUnity/Assets/_ScriptsOak/Core/SmoothCamera.cs
+++ LDUnity/Assets/_ScriptsOak/Core/SmoothCamera.cs
@@ -2,34 +2,27 @@
 
 using UnityEngine;
 using System.Collections;
+using UnityEngine.U2D;
 
 public class SmoothCamera : MonoBehaviour
 {
-    public static Transform Target;
-    public float SmoothTime = 0.3F;
-    private Vector3 Velocity = Vector3.zero;
-    public static Vector3 targetPosition;
-    public static Transform camT;
-    public static Vector2 lockSize;
-    public static bool locked;
-    private const float yParalaxStep = 7;
-    void Start() { camT = Camera.main.transform; }
-    void Update()
-    {
-        if(Target == null)
-        {
-            if(CharacterPlayer.instance != null)
-            {
-                Target = CharacterPlayer.instance.gameObject.transform;
-            }
-        }
-        else
-        {
-            transform.position = (targetPosition = Vector3.SmoothDamp(targetPosition, Target.TransformPoint(new Vector3(0, 0, -10)), ref Velocity, SmoothTime));
-        }
-    }
-    public static float Parallax(float iX, float iY, float tY)
-    {
-        return iX * (1 - Mathf.Pow(2, (iY - tY) / yParalaxStep));
-    }
+	public static Transform Target;
+	public float SmoothTime = 0.3F;
+	private Vector3 Velocity = Vector3.zero;
+	public static Vector3 targetPosition;
+	public static Transform camT;
+	public static Vector2 lockSize;
+	public static bool locked;
+	private const float yParalaxStep = 7;
+	public float yOff=1;
+	// public float minSize = 6.5f;
+	public PixelPerfectCamera ppc;
+	void Start() { camT = Camera.main.transform; }
+	void Update() {
+		// ppc.assetsPPU = Mathf.Max(1,1/camera.ratio)*
+		transform.position = (targetPosition = Vector3.SmoothDamp(targetPosition, Target.TransformPoint(new Vector3(0, locked?0:2.25f+yOff*Target.position.y, -10)), ref Velocity, SmoothTime));
+	}
+	public static float Parallax(float iX, float iY, float tY) {
+		return iX * (1 - Mathf.Pow(2, (iY - tY) / yParalaxStep));
+	}
 }
