f93b995f1abf8598c2d31ec5a4a23f34f85cb226
oakwarrior <karuott321@hotmail.com>
2020-04-20 14:39:17 +0200

    Added detach/attach logic; Added snapping logic; Added locking logic;

--- LDUnity/Assets/_ScriptsOak/Game/BodyParts/BodyPartSurgery.cs
+++ LDUnity/Assets/_ScriptsOak/Game/BodyParts/BodyPartSurgery.cs
@@ -6,21 +6,57 @@ public class BodyPartSurgery : MonoBehaviour
 {
 
     [SerializeField]
-    EBodyPart Type;
+    public EBodyPart Type;
 
     [SerializeField]
     public Transform SpurtMarker;
+
+    [SerializeField]
+    public bool bIsMatch = false;
+
+    public bool bIsDetached = false;
+
+    public bool bCanBeDetached = false;
+
+    public bool bPendingAttach = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void Detach()
+    {
+        if (bIsDetached || !bCanBeDetached)
+        {
+            return;
+        }
+        bIsDetached = true;
+
+        if(GetComponentInParent<BodySurgery>() == BodySurgery.Henry)
+        {
+            BodySurgery.Henry.DetachedParts.Add(Type);
+        }
+        gameObject.transform.SetParent(gameObject.transform.parent.parent);
+        gameObject.transform.position = gameObject.transform.position + new Vector3(Random.Range(-0.5f, -0.2f), 0);
+    }
+
+    public void Attach()
+    {
+        bIsDetached = false;
+        gameObject.transform.position = Vector3.zero;
+
+        gameObject.transform.SetParent(BodySurgery.Henry.gameObject.transform);
+        gameObject.transform.localPosition = new Vector3(-2.46f, -0.61f, 0);
+
+        bCanBeDetached = false;
     }
 }
--- LDUnity/Assets/_ScriptsOak/Game/BodyParts/BodySurgery.cs
+++ LDUnity/Assets/_ScriptsOak/Game/BodyParts/BodySurgery.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class BodySurgery : MonoBehaviour
 {
+    public static BodySurgery Henry;
+    private void Awake()
+    {
+        if(bIsHenry)
+        {
+            Henry = this;
+        }
+    }
+    public bool bIsHenry = false;
+
+    public List<Transform> SnapMarkers = new List<Transform>();
+
+    public List<EBodyPart> DetachedParts = new List<EBodyPart>();
+
+    public bool CanAttach(BodyPartSurgery part)
+    {
+        return DetachedParts.Contains(part.Type) && part.bIsMatch;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +33,10 @@ public class BodySurgery : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public Vector3 GetSnapPositionForBodyPart(EBodyPart part)
+    {
+        return SnapMarkers[(int)part].position;
     }
 }
